8d77642762153cc33358343793317f73a40dba57
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-08-30 17:00:09 +0300

    Add files via upload

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -5,7 +5,7 @@ from petstagram.photos import views
 urlpatterns = [
     path('add/', views.add_photo, name='add photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details),
-        path('edit/', views.edit_photo)
+        path('', views.photo_details, name='photo details'),
+        path('edit/', views.edit_photo, name='edit photo')
     ]))
 ]
