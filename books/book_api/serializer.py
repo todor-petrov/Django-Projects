1f4b4f10488342bbc065a1e5e9d02354766f908b
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-28 14:06:28 +0300

    Add files via upload

---
+++ books/book_api/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    number_of_pages = serializers.IntegerField()
+    publish_date = serializers.DateField()
+    quantity = serializers.IntegerField()
---
+++ books/book_api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from book_api.models import Book
+from book_api.serializer import BookSerializer
+
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
