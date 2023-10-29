31632ae28c74fbd4caebb42cb7ca03d2541735d2
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-29 05:50:01 +0200

    Add files via upload

--- books/book_api/serializer.py
+++ books/book_api/serializer.py
@@ -1,20 +1,44 @@
+# from rest_framework import serializers
+# from book_api.models import Book
+
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField()
+#     number_of_pages = serializers.IntegerField()
+#     publish_date = serializers.DateField()
+#     quantity = serializers.IntegerField()
+
+#     def create(self, data):
+#         return Book.objects.create(**data)
+    
+#     def update(self, instance, data):
+#         instance.title = data.get('title', instance.title)
+#         instance.number_of_pages = data.get('number_of_pages', instance.number_of_pages)
+#         instance.publish_date = data.get('publish_date', instance.publish_date)
+#         instance.quantity = data.get('quantity', instance.quantity)
+#         instance.save()
+#         return instance
+
 from rest_framework import serializers
 from book_api.models import Book
+from django.forms import ValidationError
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    number_of_pages = serializers.IntegerField()
-    publish_date = serializers.DateField()
-    quantity = serializers.IntegerField()
+class BookSerializer(serializers.ModelSerializer):
+    description = serializers.SerializerMethodField()
+    class Meta:
+        model = Book
+        fields = '__all__'
 
-    def create(self, data):
-        return Book.objects.create(**data)
+    def validate_title(self, value):
+        if value == 'Diet Coke':
+            raise ValidationError('No Diet Coke, Please!')
+        return value
+
+    def validate(self, data):
+        if data['number_of_pages'] > 200 and data['quantity'] > 200:
+            raise ValidationError('Too Heavy For Inventory!')
+        return data
     
-    def update(self, instance, data):
-        instance.title = data.get('title', instance.title)
-        instance.number_of_pages = data.get('number_of_pages', instance.number_of_pages)
-        instance.publish_data = data.get('publish_data', instance.publish_data)
-        instance.quantity = data.get('quantity', instance.quantity)
-        instance.save()
-        return instance
+    def get_description(self, data):
+        return 'This book is called ' + data.title + ' and it is ' + str(data.number_of_pages) + ' pages long.'
+    
--- books/book_api/urls.py
+++ books/book_api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from book_api.views import book_list, book_create, book
+from book_api.views import BookList, BookCreate, BookDetail
 
 urlpatterns = [
-    path('', book_create),
-    path('list/', book_list),
-    path('<int:pk>', book)
+    path('', BookCreate.as_view()),
+    path('list/', BookList.as_view()),
+    path('<int:pk>', BookDetail.as_view())
 ]
