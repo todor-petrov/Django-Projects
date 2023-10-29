9ace206420eea70407ec35c9456668bdac13d0c7
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-29 05:57:59 +0200

    Add files via upload

--- books/book_api/views.py
+++ books/book_api/views.py
@@ -44,3 +44,17 @@
 #         book.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
+from rest_framework.views import APIView
+from book_api.models import Book
+from book_api.serializer import BookSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class BookList(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+
