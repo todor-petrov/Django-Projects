39beffe3b6f0a4e953a0ba41eaf01085de6ad301
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-10-29 06:12:47 +0200

    Add files via upload

--- books/book_api/views.py
+++ books/book_api/views.py
@@ -58,3 +58,14 @@ class BookList(APIView):
         return Response(serializer.data)
 
 
+class BookCreate(APIView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
