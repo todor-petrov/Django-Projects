ce72dae4464ed7b847e531d8a0e0103c763462ed
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-04 13:36:25 +0200

    Add files via upload

---
+++ dcrm/dcrm/mydb.py
@@ -0,0 +1,12 @@
+# import mysql.connector
+
+# dataBase = mysql.connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     passwd = 'password'
+# )
+
+# cursorObject = dataBase.cursor()
+
+# cursorObject.execute('CREATE DATABASE elderco')
+print('All done!')
