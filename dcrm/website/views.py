f112a1201b6baec9e3b9116542784210f037336d
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-04 16:59:54 +0200

    Add files via upload

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
+    
     return render(request, 'home.html', {})
+
+def logout_user(request):
+    ...
