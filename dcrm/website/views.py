a74c4212841b022cb5843c45c54b0ba8849a5a15
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-04 17:51:03 +0200

    Add files via upload

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You Have Been Logged In, Mate!')
+            return redirect('home')
+        else:
+            messages.success(request, 'There Was An Error Logging In, Please Try Again...')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 def logout_user(request):
     ...
