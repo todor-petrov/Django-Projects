13c29d8fbaca63500c0643e6867036209d36d2e3
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-07 17:00:32 +0200

    Add files via upload

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from . forms import SignUpForm
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -16,7 +19,7 @@ def home(request):
             messages.success(request, 'There Was An Error Logging In, Please Try Again...')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
@@ -28,7 +31,6 @@ def register_user(request):
 		form = SignUpForm(request.POST)
 		if form.is_valid():
 			form.save()
-			# Authenticate and login
 			username = form.cleaned_data['username']
 			password = form.cleaned_data['password1']
 			user = authenticate(username=username, password=password)
@@ -40,3 +42,11 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+      if request.user.is_authenticated:
+            customer_record = Record.objects.get(id=pk)
+            return render(request, 'record.html', {'customer_record':customer_record})
+      else:
+            messages.success(request, "You Must Be Logged To View That Page...")
+            return redirect('home')
