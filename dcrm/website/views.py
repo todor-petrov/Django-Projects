50f149e5abfe4b0c2625aba6099b56db0bff77d0
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-07 14:33:00 +0200

    Add files via upload

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -24,14 +24,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, 'You Have Successfully Registered! Welcome!')
-            return redirect('home')
-    return render(request, 'register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
