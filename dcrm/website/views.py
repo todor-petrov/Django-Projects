7ddef305e28b9e2ac893ac9bef9df85b57eeac9f
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-08 11:50:29 +0200

    Add files via upload

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -50,3 +50,13 @@ def customer_record(request, pk):
       else:
             messages.success(request, "You Must Be Logged To View That Page...")
             return redirect('home')
+      
+def delete_record(request, pk):
+      if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+      else:
+        messages.success(request, "You Must Be Logged To Do That...")
+        return redirect('home')
