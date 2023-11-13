b0e94cda3d4439ac80e7a9197d4a5ffc8df82168
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-13 11:27:56 +0200

    Add files via upload

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -5,6 +5,9 @@ import datetime
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
