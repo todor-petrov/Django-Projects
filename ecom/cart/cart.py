4d7033263b8269ed9e38ef8ec6886c538294e5e0
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-14 14:58:03 +0200

    Add files via upload

---
+++ ecom/cart/cart.py
@@ -0,0 +1,7 @@
+class Cart():
+    def __init__(self, request) -> None:
+        self.session = request.session
+        cart = self.session.get('session_key')
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+        self.cart = cart
