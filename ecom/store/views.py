3936b1d97e6950f53553d39b2174f6dd02ddfb04
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-14 12:53:23 +0200

    Add files via upload

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request, 'That Category Does Not Exist!')
+        return redirect('home')
+    
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
