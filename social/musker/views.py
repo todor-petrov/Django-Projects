6adf2caf9cafd92ab315e4f45b89bf33f6dd8e1c
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-21 14:56:34 +0200

    Add files via upload

--- social/musker/views.py
+++ social/musker/views.py
@@ -17,7 +17,22 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        # Post Form Logic
+        if request.method == 'POST':
+            # Get current user
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Deside to follow or unfollow
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            # Save the profile
+            current_user_profile.save()
+
         return render(request, 'profile.html', {'profile':profile})
     else:
         messages.success(request, 'You Must Be Logged In To View This Page...')
         return redirect('home')
+
