5ff4f609b7b9245b89cb14b938348425f492c86c
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-23 14:55:16 +0200

    Add files via upload

---
+++ social/musker/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Meep
+
+
+class MeepForm(forms.ModelForm):
+    body = forms.CharField(
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Enter Your Musker Meep!',
+                'class': 'form-control',
+                }
+                ),
+                label='',)
+    
+    class Meta:
+        model = Meep
+        exclude = ('user',)
