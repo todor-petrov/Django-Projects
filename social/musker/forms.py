1aa95d4364084232d1a97e06299e908e1dd5d001
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-28 11:59:17 +0200

    Add files via upload

--- social/musker/forms.py
+++ social/musker/forms.py
@@ -6,9 +6,17 @@ from django.contrib.auth.models import User
 
 class ProfilePicForm(forms.ModelForm):
     profile_image = forms.ImageField(label='Profile Picture')
+
+    profile_bio = forms.CharField(label='Profile Bio', widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Profile Bio'}))
+    homepage_link = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Website Link'}))
+    facebook_link = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Facebook Link'}))
+    instagram_link = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Instagram Link'}))
+    linkedin_link = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'LinkedIn Link'}))
+
+
     class Meta:
         model = Profile
-        fields = ('profile_image',)
+        fields = ('profile_image', 'profile_bio', 'homepage_link', 'facebook_link', 'instagram_link', 'linkedin_link', )
 
 
 class MeepForm(forms.ModelForm):
--- social/musker/models.py
+++ social/musker/models.py
@@ -21,6 +21,12 @@ class Profile(models.Model):
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
     date_modified = models.DateTimeField(User, auto_now=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='images/')
+    
+    profile_bio = models.CharField(null=True, blank=True, max_length=500)
+    homepage_link = models.CharField(null=True, blank=True, max_length=100)
+    facebook_link = models.CharField(null=True, blank=True, max_length=100)
+    instagram_link = models.CharField(null=True, blank=True, max_length=100)
+    linkedin_link = models.CharField(null=True, blank=True, max_length=100)
 
     def __str__(self):
         return self.user.username
