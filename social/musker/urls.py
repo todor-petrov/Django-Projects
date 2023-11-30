155a0aec9218508d103894c26ef85585e7c15519
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-11-30 15:33:00 +0200

    Add files via upload

--- social/musker/urls.py
+++ social/musker/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
      path('profile_list/', views.profile_list, name='profile_list'),
+     path('profile/followers/<int:pk>', views.followers, name='followers'),
      path('profile/<int:pk>', views.profile, name='profile'),
      path('login/', views.login_user, name='login'),
      path('logout/', views.logout_user, name='logout'),
