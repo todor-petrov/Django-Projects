46985b730017930b269b0d801f29df8091b48aed
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-01 13:50:49 +0200

    Add files via upload

--- social/musker/urls.py
+++ social/musker/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
      path('', views.home, name='home'),
      path('profile_list/', views.profile_list, name='profile_list'),
      path('profile/followers/<int:pk>', views.followers, name='followers'),
+     path('profile/follows/<int:pk>', views.follows, name='follows'),
      path('profile/<int:pk>', views.profile, name='profile'),
      path('login/', views.login_user, name='login'),
      path('logout/', views.logout_user, name='logout'),
@@ -14,4 +15,5 @@ urlpatterns = [
      path('meep_show/<int:pk>', views.meep_show, name='meep_show'),
      path('unfollow/<int:pk>', views.unfollow, name='unfollow'),
      path('follow/<int:pk>', views.follow, name='follow'),
+     
 ]
