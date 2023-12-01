979762d9e915b2bbdf06914e0e204128ac469a8b
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-01 17:47:34 +0200

    Add files via upload

--- social/musker/views.py
+++ social/musker/views.py
@@ -218,3 +218,13 @@ def edit_meep(request, pk):
 	else:
 		messages.success(request, 'Please, Log In To Continue...')
 		return redirect('home')
+
+def search(request):
+	if request.method == 'POST':
+		search = request.POST['search']
+		searched = Meep.objects.filter(body__contains = search)
+		return render(request, 'search.html', {'search': search, 'searched': searched})
+
+
+	else:
+		return render(request, 'search.html', {})
