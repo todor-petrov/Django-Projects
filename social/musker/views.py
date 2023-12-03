83589353a83ef94968a08b95a687e48636087207
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2023-12-03 09:40:02 +0200

    Add files via upload

--- social/musker/views.py
+++ social/musker/views.py
@@ -228,3 +228,13 @@ def search(request):
 
 	else:
 		return render(request, 'search.html', {})
+	
+def search_user(request):
+	if request.method == 'POST':
+		search = request.POST['search_user']
+		searched = User.objects.filter(username__contains = search)
+		return render(request, 'search_user.html', {'search': search, 'searched': searched})
+
+
+	else:
+		return render(request, 'search_user.html', {})
